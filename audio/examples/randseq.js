bfefb62440f4c4935ad195370d445b20d3ea9f57
m-onz <monz@startmail.com>
2022-03-22 15:22:09 +0000

    ...

---
+++ audio/examples/randseq.js
@@ -0,0 +1,32 @@
+var inlets = 4
+
+var arr = []
+var limit = 16
+var max = 90
+var restAmount = 20
+
+function getNote () {
+  if (Math.random() * 100 > restAmount) {
+    return Math.floor(Math.random()*max)
+  } else {
+    return '-'
+  }
+}
+
+function bang () {
+  arr = []
+  for (var i = 0; i < limit; i++) {
+    arr.push(getNote());
+  }
+  outlet(0, arr)
+}
+
+function msg_float (num) {
+  if (inlet === 1) {
+    max = num
+  } else if (inlet === 2) {
+    restAmount = num
+  } else if (inlet === 3) {
+    limit = num
+  }
+}
